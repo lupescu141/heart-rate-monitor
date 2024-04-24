53b280d2d5e98972c1eecb1943f591fcda9b9b67
Daniel Lupescu <d.lupescu@hotmail.fi>
2024-04-24 16:29:42 +0300

    Update main.py
    added class HeartMonitor and initializer.

--- main.py
+++ main.py
@@ -5,15 +5,31 @@ from ssd1306 import SSD1306_I2C
 import micropython
 micropython.alloc_emergency_exception_buf(200)
 
-sw0 = Pin(9, Pin.IN, Pin.PULL_UP)
-sw1 = Pin(8, Pin.IN, Pin.PULL_UP)
-sw2 = Pin(7, Pin.IN, Pin.PULL_UP)
-i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
-oled_width = 128
-oled_height = 64
-oled = SSD1306_I2C(oled_width, oled_height, i2c)
-
 
 class HeartMonitor():
     
+    def __init__(self):
+        
+        # Hardware:
+            
+                # Buttons:
+            
+        self.sw0 = Pin(9, Pin.IN, Pin.PULL_UP)
+        self.sw1 = Pin(8, Pin.IN, Pin.PULL_UP)
+        self.sw2 = Pin(7, Pin.IN, Pin.PULL_UP)
+        
+                # Display Settings:
+        
+        self.i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+        self.oled_width = 128
+        self.oled_height = 64
+        self.oled = SSD1306_I2C(oled_width, oled_height, i2c)
+        
+                # Attributes:
     
+    def mainMenu(self):
+        
+    def beats_per_minute(self):
+    
+        
+        
