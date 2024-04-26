32d9017eff1815798238f2a49b29010fe0461289
DancingSoldier <142883434+DancingSoldier@users.noreply.github.com>
2024-04-26 18:49:41 +0300

    Update main.py
    Joonatan Branch

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
-
-
+        # Sensor
+sensor = ADC(26)
+sensor_history_size = 250
+history = []
 class HeartMonitor():
     
     def __init__(self):
@@ -39,6 +41,8 @@ class HeartMonitor():
         self.menuTxtKubios 	= "Kubios"
         
         # measure_heart_rate attributes:
+        self.beats = 0
+        self.beat = False
         
         # basic_hrv_analysis attributes:
         
@@ -53,7 +57,7 @@ class HeartMonitor():
         
         self.dataset = { 
                     "type": "RRI", 
-                    "data": intervals, 
+                    "data": "intervals", 
                     "analysis": {"type": "readiness"}
                     } 
         
@@ -61,25 +65,43 @@ class HeartMonitor():
         
     
     def mainMenu(self):
-        
+        pass
         
     def measure_heart_rate(self):
+        global history
+        sensor_data = sensor.read_u16()
+        history.append(sensor_data)
+        history = history[-sensor_history_size:]
+        
+        floor, roof = min(history), max(history)
+        
+        max_filter = (floor + roof * 3)//4
+        min_filter = (floor + roof)//2
+        
+        
+        if not self.beat and sensor_data > max_filter:
+            self.beat = True
+            self.beats += 1
+            print(f"beat: {self.beat}, beats:{self.beats}, raw value: {sensor_data}")
+        if self.beat and sensor_data < min_filter:
+            self.beat = False
+            print(f"beat: {self.beat}, beats:{self.beats}, raw value: {sensor_data}")
         
         
     def basic_hrv_analysis(self):
-        
+        pass
         
     def history(self):
-        
+        pass
         
     def kubios(self):
-        
+        pass
         
     def sendData_MQTT(self):
-        
+        pass
         
 # Main Loop:
-
+device = HeartMonitor()
 while True:
-    
+    device.measure_heart_rate()
         
