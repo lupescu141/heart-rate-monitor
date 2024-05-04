66e68733fc85ee749d4a7c135413fe232690d878
Daniel Lupescu <d.lupescu@hotmail.fi>
2024-05-04 12:35:38 +0300

    Update main.py
    Added a boolean to check if buttons are up. made device into a state machine. Now you can go back from heart rate measure to main menu.

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ oled = SSD1306_I2C(oled_width, oled_height, i2c)
 
         # Sensor:
 
-
-
 sensor = ADC(26)
 sensor_history_size = 250
 history = []
@@ -41,6 +39,8 @@ class HeartMonitor():
     def __init__(self):
         
         # main attribtes:
+        self.deviceState = "Main menu"
+        self.buttonsAreUp = True
         
         # mainMenu attributes:
         
@@ -75,7 +75,12 @@ class HeartMonitor():
                     } 
         
         # sendData_MQTT attributes:
+    
+    def checkButtonsAreUp(self):
         
+        if sw0() == 1 and sw1() == 1 and sw2() == 1:
+            
+            self.buttonsAreUp = True
     
     def mainMenu(self):
 
@@ -90,47 +95,56 @@ class HeartMonitor():
             
             oled.fill_rect(0, 0, 10, 64, 0)
             oled.text(self.menuPointer, 2, 2, 1)
+            
 
         elif self.menuState == 2:
             
             oled.fill_rect(0, 0, 10, 64, 0)
             oled.text(self.menuPointer, 2, 15, 1)
             
+            
         elif self.menuState == 3:
             
             oled.fill_rect(0, 0, 10, 64, 0)
             oled.text(self.menuPointer, 2, 28, 1)
+            
 
         elif self.menuState == 4:
             
             oled.fill_rect(0, 0, 10, 64, 0)
             oled.text(self.menuPointer, 2, 41, 1)
             
-        if sw2() == 0:
+            
+        if sw2() == 0 and  self.buttonsAreUp == True:
             
             if self.menuState == 1:
                 
                 self.menuState = 4
+                self.buttonsAreUp = False 
             
             else:
                 
                 self.menuState -= 1
+                self.buttonsAreUp = False 
                 
-        if sw0() == 0:
+        if sw0() == 0 and  self.buttonsAreUp == True:
             
             if self.menuState == 4:
                 
                 self.menuState = 1
+                self.buttonsAreUp = False 
             
             else:
                 
                 self.menuState += 1
+                self.buttonsAreUp = False 
                 
-        if sw1() == 0:
+        if sw1() == 0 and self.buttonsAreUp == True:
             
             if self.menuState == 1:
+                
                 timer.init(period=10000, mode=Timer.PERIODIC, callback=timer_callback)			#Timer päälle BPM laskua varten
-                self.measure_heart_rate()					#Sensorin käynnistys napista testausta varten
+                self.deviceState = "Measure heart rate"
                 
             elif self.menuState == 2:
                 pass
@@ -141,6 +155,8 @@ class HeartMonitor():
             elif self.menuState == 4:
                 pass
         
+        self.checkButtonsAreUp()
+        
     def measure_heart_rate(self):
 
         global history
@@ -149,10 +165,14 @@ class HeartMonitor():
         
         oled.text("BPM: ", 15, 50, 1)
         oled.show()
-        if sw1() == 0 and sensor_bool == False:		#sensori menee päälle nappia painamalla sen jälkeen kun menusta on valittu ittaus
+        
+        if sw1() == 0 and sensor_bool == False:		#sensori menee päälle nappia painamalla sen jälkeen kun menusta on valittu mittaus
             sensor_bool = True
+            
         else:
             sensor_bool = False						#Jostain syystä mittauksen lopetus ei toimi kun nappia painaa uudestaan
+            
+            
         while sensor_bool == True:
 
             sensor_data = sensor.read_u16()
@@ -173,19 +193,26 @@ class HeartMonitor():
                 self.beat = False
                 print(f"beat: {self.beat}, beats:{self.beats}, raw value: {sensor_data}")
                 
+            if sw1() == 0 and self.buttonsAreUp == True:
+            
+                self.deviceState = "Main menu"
+                break
+            
+            self.checkButtonsAreUp()
             time.sleep_ms(2)
-                
+            
         
     def bpm_calc(self):
+        
         oled.fill(0)
         print("callback")
         bpm = self.beats * 12
-
         self.beats = 0
         self.UserBPMText = "BPM: "+ str(bpm)
         oled.text(self.UserBPMText, 15, 50, 1)
         print(bpm)
         oled.show()
+        
         
     def basic_hrv_analysis(self):
         pass
@@ -197,20 +224,40 @@ class HeartMonitor():
         
     def kubios(self):
         pass
-        
-    def history(self):
-        pass
-            
+                   
+                   
     def sendData_MQTT(self):
         pass
 
+
 # Main Loop:
 device = HeartMonitor()
+
 # Timer callback
 def timer_callback(timer):
     
     device.bpm_calc()
 
+
 while True:
-    device.mainMenu()
+    
+    if device.deviceState == "Main menu":
+        
+        device.mainMenu()
+        
+    if device.deviceState == "Measure heart rate":
+        
+        device.measure_heart_rate()
+        
+    if device.deviceState == "Basic hrv analysis":
+        
+        device.basic_hrv_analysis()
+        
+    if device.deviceState == "History":
+        
+        device.history()
+        
+    if device.deviceState == "kubios":
+        
+        device.kubios()
     
